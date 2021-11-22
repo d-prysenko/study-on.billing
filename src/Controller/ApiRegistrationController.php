57ce8b80f62d36e75024c05d975d14fdfcc55968
Dmitriy Prysenko <d.prysenko@ya.ru>
2021-11-22 11:24:41 +0300

    docs added

--- src/Controller/ApiRegistrationController.php
+++ src/Controller/ApiRegistrationController.php
@@ -21,13 +21,39 @@ use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 class ApiRegistrationController extends AbstractController
 {
     /**
-     * @Route(name="api_v1_register", path="/api/v1/register")
      * @OA\Post(
-     *     path="/api/v1/register",
-     *     method="POST",
-     *     summary="Register user",
-     *     description="kekw"
+     *     summary="Registrates new user",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="username",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string"
+     *                 ),
+     *                 example={"username": "user@study.on", "password": "SoHardPassword123"}
+     *             )
+     *         )
+     *     ),
      *
+     *  @OA\Response(
+     *     response="200",
+     *     description="JW token",
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         @OA\Property(
+     *           property="token",
+     *           type="string"
+     *         ),
+     *        example={"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2Mzc1NDk2NTQsImV4cCI6MTYzNzU1MzI1NCwicm9sZXMiOlsiUk9MRV9TVVBFUl9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6InN1cGVyX2FkbWluQGVtYWlsLmNvbSJ9.Df2fBZirWHynelRY-Ystv675Tm6_YsGMdRE6AhCUm_XSgiArD7LXrLucKl73xRVa4cxKHLICqEvLuViv4vgdOUTBDBeg81Aa0EdlA1G6wkwTVxuBUIS1q-iI1Xm9B3b-p08kxsLdbDBR3f_2l83XXdB6gJnOcesgPm9szJT9vyGgfjLTzi2-DTXr0u_2sLgiK55ouCW5FzFTp8JGwwrkDg4jq6RRwr7vWarlc4nDSGaVoqpHe7s2HByeiSa7aZRlWeZrzrTIscGgav1N_6MY7je9Hm6mSblSzBDi63bFS9sd-kBw7O_XZGA2gcoIZZj4YJ1cTvFryYrf-IPTVZQVaj8UjcujZE4-Ck_GyvZghW2F25ls4b8jJDe1kGGYkDGStFNPoi29Hw8j-emnhFr9orj8lMZesU5LWH2j7nNsQQUAnqQeBHtJhPSPl9c0muK6U5Ds9mnMHY0-5iRVjGTtF_6WgDr5qQfX1GNOuquhO6EN1kdo6SHgPIOpwzAM9ygrffi6zqO7SSRVQ-mK3hOJqu_cIG1BHn5Dopwvnh_MeL63x090_nrcmGyoHs2h2xj1bHPBAYYez61vzwagRfROEcrOsZYR4R8QpDgut4TRUUI2jgkcXFArUiWcaXOgP3NKwGfshnkLd809zb609o2ETMoMlLrQssiUWvrjOffZyf8"}
+     *       )
+     * )
+     * )
      * )
      * @Security(name="Bearer")
      */
--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -5,13 +5,56 @@ namespace App\Controller;
 use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
+use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
 
 class ApiUserController extends AbstractController
 {
+    /**
+     *
+     * @OA\Get(
+     *     summary="Returns info for user",
+     *  @OA\Response(
+     *     response="200",
+     *     description="JW token",
+     *
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         @OA\Property(
+     *           property="code",
+     *           type="int"
+     *         ),
+     *         @OA\Property(
+     *           property="username",
+     *           type="string"
+     *         ),
+     *         @OA\Property(
+     *           property="roles",
+     *           type="array",
+     *           @OA\Items(items={"ROLE_USER", "ROLE_ADMIN"})
+     *         ),
+     *         @OA\Property(
+     *           property="balance",
+     *           type="float"
+     *         ),
+     *        example={"code": 200, "username": "user@test.com", "roles": {"ROLE_USER", "ROLE_SUPER_USER"}, "balance": 20.0}
+     *       )
+     * )
+     * )
+     * )
+     * * @OA\Parameter(
+     *     name="Authorization",
+     *     in="header",
+     *     description="Header must contain JWT Authorization token",
+     *     @OA\Schema(type="string"), example="Bearer <token>"
+     * )
+     * @Security(name="Bearer")
+     */
     public function current(Request $request, JWTEncoderInterface $jwtEncoder): Response
     {
         try {
